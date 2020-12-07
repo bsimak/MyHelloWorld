20002e32d23fd280e5b5ad894b624016f088762b
B.simak <b.simak@gmail.com>
2020-12-07 16:29:42 +0100

    Show current time

--- src/main/java/MyHelloWorld.java
+++ src/main/java/MyHelloWorld.java
@@ -1,15 +1,29 @@
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class MyHelloWorld {
     public static void main(String[] args) {
 
         String frage1 = "Hello, what's your name?";
-        String nobody = "nobody";
 
+        String jetzt = wieSpaet();
+        zeigeUhrzeit(jetzt);
         gehFragen(frage1);
         String myName = gibAntwort();
         gehAntworten(myName);
     }
+        static String wieSpaet() {
+            Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+            String minutef = String.format("%02d", minute);
+            String jetzt = hour + ":" + minutef;
+            return(jetzt);
+    }
+        static void zeigeUhrzeit(String jetzt) {
+            System.out.println("Jetzt ist es: " + jetzt );
+        }
+
         static void gehFragen(String frage1) {
             System.out.println(frage1);
         }
