f118589f8a2c40ae859a68cdc6964be1ffe4fbbb
B.simak <b.simak@gmail.com>
2020-12-07 17:51:55 +0100

    Sleep eingebaut aber noch Fehler bei der Abfrage der Eingabe (scan) - Programm macht noch nicht was es soll

--- src/main/java/MyHelloWorld.java
+++ src/main/java/MyHelloWorld.java
@@ -1,15 +1,18 @@
 import java.util.Calendar;
 import java.util.Scanner;
+import java.lang.Thread;
 
 public class MyHelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         String frage1 = "Hello, what's your name?";
+        String nobody = "nobody";
+        int dauer = 10000;
 
         String jetzt = wieSpaet();
         zeigeUhrzeit(jetzt);
         gehFragen(frage1);
-        String myName = gibAntwort();
+        String myName = gibAntwort(nobody, dauer);
         gehAntworten(myName);
     }
         static String wieSpaet() {
@@ -28,14 +31,16 @@ public class MyHelloWorld {
         static void gehFragen(String frage1) {
             System.out.println(frage1);
         }
-        static String gibAntwort() {
+        static String gibAntwort(String nobody, int dauer) throws InterruptedException {
             Scanner scan = new Scanner(System.in);
-            String name = scan.next();
+            String name = nobody;
+            name = scan.next();
             scan.close();
+            Thread.sleep(dauer);
             return(name);
         }
         static void gehAntworten(String myName) {
             System.out.println("Hello " + myName + ", still busy?");
         }
-    }
+}
 
